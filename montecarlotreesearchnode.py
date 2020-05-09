e7727d910919c3cec3d598a60f297a5dea9e6654
shehio <shehabyasser@gmail.com>
2020-05-09 10:52:07 -0700

    Added back prop

--- montecarlotreesearchnode.py
+++ montecarlotreesearchnode.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
-
 import numpy as np
+
+from twoplayergame import GameState
 
 
 class MonteCarloTreeSearchNode:
-    def __init__(self, game_state, parent):
+    def __init__(self, game_state: GameState, parent: MonteCarloTreeSearchNode):
         self.game_state = game_state
         self.parent = parent
         self.children = np.array([], dtype=MonteCarloTreeSearchNode)
         self.wins = 0
-        self.number_of_visits = 0
+        self.visits = 0
 
     def select(self) -> MonteCarloTreeSearchNode:
         pass
@@ -20,21 +21,25 @@ class MonteCarloTreeSearchNode:
     def rollout(self) -> float:
         pass
 
-    def backpropagate(self):
-        pass
+    def backpropagate(self, win):
+        self.visits += 1
+        self.wins += win
+        if self.parent is not None:
+            self.parent.backpropagate(win)
 
-    def select_child_with_max_ucb(c):
-        pass
+    def select_child_with_max_ucb(self, c):
+        ucb_values = list(map(lambda child: MonteCarloTreeSearchNode.get_ucb(child, c), self.children))
+        return self.children(np.argmax(ucb_values))
 
     @property
     def win_ratio(self):
         # If the node hasn't been visited, then the win_ratio (part of ucb) is inf. This means it will be selected.
-        if self.number_of_visits == 0:
+        if self.visits == 0:
             return np.inf
-        return self.wins / self.number_of_visits
+        return self.wins / self.visits
 
     @staticmethod
     def get_ucb(child: MonteCarloTreeSearchNode, c):
-        child.win_ratio + c * np.sqrt(np.log(child.parent.number_of_visits) / child.number_of_visits)
+        child.win_ratio + c * np.sqrt(np.log(child.parent.visits) / child.visits)
 
 
--- twoplayergame.py
+++ twoplayergame.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class GameState:
     def __init__(self):
         pass
