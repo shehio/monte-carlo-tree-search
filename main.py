ac29ca8384a903aeda669ab67df657c68a1d84af
shehio <shehabyasser@gmail.com>
2020-05-09 00:46:28 -0700

    Added a skeleton for main.

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from twoplayergame import GameState
+from montecarlotreesearch import MonteCarloTreeSearch
+
+import numpy as np
+import random
+
 if __name__ == '__main__':
-    print('Hello World!')
+    game = GameState()
+    p1 = MonteCarloTreeSearch(game)
+
+    while game.is_game_over() == np.inf:
+        p1_move = p1.get_best_move(game)
+        game = game.make_move('p1', p1_move)
+
+        p2_move = game.make_move(random.choice(game.get_legal_actions()))
+        game = game.mak_move('p2', p2_move)
+
+    print(game)
--- twoplayergame.py
+++ twoplayergame.py
@@ -2,7 +2,7 @@ class GameState:
     def __init__(self):
         pass
 
-    def make_move(self, player, move):  # Returns a game state.
+    def make_move(self, player, move) -> GameState:  # Returns a game state.
         pass
 
     def is_game_over(self):  # Say returns -1 for p1 win, 1 for p2 win, 0 for draw, and -inf for game still in progress.
