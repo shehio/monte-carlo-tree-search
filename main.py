03359c89dcfb76cae74f1ddfbfd31649a3287cd8
shehio <shehabyasser@gmail.com>
2020-05-09 11:54:48 -0700

    Integrated player in main

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from twoplayergame import GameState
 from montecarlotreesearch import MonteCarloTreeSearch
+from player import Player
 
+from functools import partial
 import numpy as np
 import random
 
 if __name__ == '__main__':
     game = GameState()
-    p1 = MonteCarloTreeSearch(game)
+
+    mcts_strategy = partial(MonteCarloTreeSearch.get_best_move, number_of_simulation=100)
+    p1 = Player('p1', mcts_strategy)
+
+    p2 = Player('p2', lambda game_state: random.choice(game_state.get_legal_actions()))
 
     while game.is_game_over() == np.inf:
-        p1_move = p1.get_best_move(game)
-        game = game.make_move('p1', p1_move)
-
-        p2_move = game.make_move(random.choice(game.get_legal_actions()))
-        game = game.mak_move('p2', p2_move)
-
-    print(game)
+        game = game.make_move(p1.make_move(game))
+        game = game.make_move(p2.make_move(game))
+        print(game)
--- montecarlotreesearch.py
+++ montecarlotreesearch.py
@@ -5,14 +5,12 @@ from montecarlotreesearchnode import MonteCarloTreeSearchNode
 
 
 class MonteCarloTreeSearch:
-    def __init__(self, game_state: GameState):
-        self.game_state = game_state
-        self.root = MonteCarloTreeSearchNode(game_state)
-
     # Note that there's no training / testing here.
     # Only get the best move to make according to simulation, regardless of how deep the actual game tree is.
     # Note that, at number_of_simulation = inf, MCTS --> Minimax.
-    def get_best_move(self, number_of_simulation=100):
+    @staticmethod
+    def get_best_move(self, game_state: GameState, number_of_simulation=100):
+        root = MonteCarloTreeSearchNode(game_state)
         for i in range(number_of_simulation):
             print(f'Iteration number: {i + 1}')
             leaf_node = self.root.select(c=np.sqrt(2))
