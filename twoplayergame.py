2618c173f7b8a78c614ca448f5def0843761c57c
shehio <shehabyasser@gmail.com>
2020-05-09 00:34:08 -0700

    Defined the game's contract

---
+++ twoplayergame.py
@@ -0,0 +1,12 @@
+class GameState:
+    def __init__(self):
+        pass
+
+    def make_move(self, player, move):  # Returns a game state.
+        pass
+
+    def is_game_over(self):  # Say returns -1 for p1 win, 1 for p2 win, 0 for draw, and -inf for game still in progress.
+        pass
+
+    def get_legal_actions(self, player):
+        pass
